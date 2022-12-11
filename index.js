37e2113b7ba5d51a7596d8eb800008edcfe2b30f
khRasikh <kh.rasikh542@gmail.com>
2022-12-11 19:25:52 +0100

    chore: adding swagger documentation

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
-const dotenv = require('dotenv'); 
-dotenv.config(); 
-
-const port = process.env.PORT
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const dotenv = require("dotenv");
+const swaggerUI = require("swagger-ui-express");
+const swaggerJSON = require("./swagger.json");
+dotenv.config();
+const port = process.env.PORT;
 
 app.get("/payment", (req, res) => {
   res.send("Welcome to Payment Endpoint!");
 });
+
+app.use(
+  "/",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerJSON, {
+    explorer: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
---
+++ payment/index.js
@@ -0,0 +1,8 @@
+const stripe = require("stripe")("sk_test_...");
+
+stripe.customers
+  .create({
+    email: "customer@example.com",
+  })
+  .then((customer) => console.log(customer.id))
+  .catch((error) => console.error(error));
